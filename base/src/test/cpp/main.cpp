b74e892a8bc06084c21157acfab9abce537c4471
Al <alastairpatrick@gmail.com>
2021-05-10 11:50:13 -0700

    Try to fix Macos OpenJ9 VM initialization failure

--- base/src/test/cpp/main.cpp
+++ base/src/test/cpp/main.cpp
@@ -4,10 +4,8 @@
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
 
-    const jint num_vm_args = 3;
+    const jint num_vm_args = 1;
     static const char* vm_args[num_vm_args] = {
-        "-Xms1024M",
-        "-Xmx2048M",  // otherwise CI tests fail on MacOS with OpenJ9 VM with JVMJ9VM015W Initialization error for library j9gc29(2): Failed to instantiate compressed references metadata.  200M requested
         "-Xcheck:jni"
     };
     whatjni::initialize_vm(JNI_VERSION_1_8, num_vm_args, vm_args);
